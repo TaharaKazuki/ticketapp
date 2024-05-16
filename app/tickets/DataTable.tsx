cb958ecfc3fd5138d6814447a5461dd7ead5f27d
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-16 17:08:27 +0900

    update

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -14,29 +14,40 @@ type Props = {
 
 const DataTable = ({ tickets }: Props) => {
   return (
-    <div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Priority</TableHead>
-            <TableHead>Created At</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {tickets
-            ? tickets.map((ticket) => (
-                <TableRow key={ticket.id} data-href="/">
-                  <TableCell>{ticket.title}</TableCell>
-                  <TableCell>{ticket.status}</TableCell>
-                  <TableCell>{ticket.priority}</TableCell>
-                  <TableCell>{ticket.createdAt.toLocaleDateString()}</TableCell>
-                </TableRow>
-              ))
-            : null}
-        </TableBody>
-      </Table>
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>Created At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-href="/">
+                    <TableCell>{ticket.title}</TableCell>
+                    <TableCell>{ticket.status}</TableCell>
+                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString('jp-JP', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: 'numeric',
+                        minute: '2-digit',
+                        hour12: true,
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
