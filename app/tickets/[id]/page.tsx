d7a2ca0b8f0bfd471e9582f732a8e138ca581d9d
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-17 14:48:34 +0900

    update

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,4 +1,17 @@
-const ViewTicket = () => {
+import prisma from '@/prisma/db';
+type Props = {
+  params: { id: string };
+};
+
+const ViewTicket = async ({ params }: Props) => {
+  const ticket = await prisma.ticket.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!ticket) {
+    return <p className="text-destructive">Ticket Not Found</p>;
+  }
+
   return <div>ViewTicket</div>;
 };
 
