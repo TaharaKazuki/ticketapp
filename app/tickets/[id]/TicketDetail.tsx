7aadd94605584c24ca5d5c55e786132fb20da5c8
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-17 15:28:17 +0900

    update

---
+++ app/tickets/[id]/TicketDetail.tsx
@@ -0,0 +1,15 @@
+import { ticket } from '@prisma/client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+const TicketDetail = () => {
+  return <div>TicketDetail</div>;
+};
+
+export default TicketDetail;
